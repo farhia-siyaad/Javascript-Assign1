21e1c70db43038dbf0e458205d5c50c9840e4712
farhia-siyaad <148630711+farhia-siyaad@users.noreply.github.com>
2023-11-20 13:30:38 +0300

    This is js Assignment 1

---
+++ main.js
@@ -0,0 +1,25 @@
+let Num1 = parseInt( prompt("Enter Num1"));
+let  Num2 = parseInt( prompt("Enter Num2"));
+
+let sum= Num1+Num2;
+document.write("your sum is  : ", +sum, "<br>");
+
+let substraction= Num1-Num2;
+document.write("your substraction is: ", +substraction , "<br>");
+
+let multiplication= Num1*Num2;
+document.write("your multiplication is: ", +multiplication , "<br>");
+
+let Exponantion= Num1**2;
+document.write("your Exponantion is: ", +Exponantion  ,"<br>");
+let Division= Num1/Num2;
+document.write("your Division is: ", + Division , "<br>");
+
+let modulus= Num1%Num2;
+document.write("your modulus is: ", + modulus , "<br>");
+
+Num1++
+Num2++
+
+document.write("your increment of num1 is: ", + Num1 ,"<br>" );
+document.write("your increment of num2 is: ", + Num2 );
